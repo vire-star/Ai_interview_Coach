0268ec7a7de3485bc8cc4b5d242411024b5b5940
Virendra pratap <virendrapratap8744@gmail.com>
2025-11-18 11:25:04 +0530

    updated hero page

--- components/hero.jsx
+++ components/hero.jsx
@@ -46,7 +46,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          <Link href="">
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
